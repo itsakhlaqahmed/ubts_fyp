97357d3d3acf843a8207405618cd300faa545706
Akhlaq Ahmed <akhlaqahmed990@gmail.com>
2024-09-02 02:18:28 +0500

    Removed unused imports

--- lib/widgets/signup/signup_form.dart
+++ lib/widgets/signup/signup_form.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:ubts_fyp/models/user.dart';
 import 'package:ubts_fyp/pages/login.dart';
-import 'package:ubts_fyp/widgets/common/custom_snackbar.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:ubts_fyp/widgets/common/text_field.dart';
 import 'package:ubts_fyp/widgets/common/wide_button.dart';
